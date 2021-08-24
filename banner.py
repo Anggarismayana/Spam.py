aac2718907731d77d2dceb7e91be2bb100d9c93e
Anggarismayana <88485408+Anggarismayana@users.noreply.github.com>
2021-08-24 16:52:27 +0700

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,16 @@
+import os,sys,time,random
+
+os.system("clear")
+
+def mengetik(s):
+
+    for c in s + '\n':
+
+        sys.stdout.write(c)
+
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+mengetik('     \033[1;32m SELAMAT DATANG HACKERS \033[1;31mINDO\033[1;37mNESIA \n\033[94;1m+---------------------------------------------+\n\033[92;1m[•] Author    : ANGGA RISMAYANA\n[•] Team      : MAFIA TEKNOLOGI\n[•] Instagram : rismayanaangga_04\n\033[94;1m\n+---------------------------------------------+\n\033[1;33m\n     PERHATIKAN !!\n\033[1;37m ➤ \033[1;33mGunakan dengan bijak ya nyet !\n\033[1;37m ➤ \033[1;33mKetik python Spam.py untuk Mulai Spam !!')
+
